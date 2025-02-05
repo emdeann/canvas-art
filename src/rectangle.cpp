35ee79ef1c5a666299c7eb0468d621a756e0b415
KatieYu <kcyu0003@zonemail.clpccd.org>
2025-02-04 20:06:40 -0800

    Katie Rectangle 2

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -6,6 +6,11 @@ Rectangle::Rectangle(const Point& _topLeft, const Point& _bottomRight) {
     Point _topRight(_bottomRight.getX(), _topLeft.getY());
     Point _bottomLeft(_topLeft.getX(), _bottomRight.getY());
 
+    points.push_back(_topLeft);
+    points.push_back(_topRight);
+    points.push_back(_bottomRight);
+    points.push_back(_bottomLeft);
+
     Line topLine(_topLeft, _topRight);
     lines.push_back(topLine);
     Line rightLine(_topRight, _bottomRight);
@@ -18,4 +23,12 @@ Rectangle::Rectangle(const Point& _topLeft, const Point& _bottomRight) {
     this -> width = abs(_topLeft.getX() - _bottomRight.getX());
     this -> height = abs(_topLeft.getY() - _bottomRight.getY());
     
+}
+
+const vector<Point> Rectangle::getPoints() const {
+    return points;
+}
+
+float Rectangle::getArea() const {
+    return width * height;
 }
