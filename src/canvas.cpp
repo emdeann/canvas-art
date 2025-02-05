a87ca00bd0f966873aae390372c51b6d2af3305e
Dean <me@deann.dev>
2025-02-04 19:35:25 -0800

    Finish implementing canvas

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -11,3 +11,17 @@ Canvas::Canvas(int width, int height, std::string author) : AbstractGrid(width, 
 void Canvas::addShape(const shared_ptr<AbstractShape> shape) {
     shapes.push_back(*shape);
 }
+
+void Canvas::draw() const {
+    AbstractGrid::draw();
+    cout << "Author: " << author << endl;
+}
+
+float Canvas::getPaintNeeded() const {
+    float paintNeeded = 0;
+    for (AbstractShape s : shapes) {
+        paintNeeded += s.getArea();
+    }
+
+    return paintNeeded;
+}
